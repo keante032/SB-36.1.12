1122471ce208400c628fbca50797b3c9d32dd973
Kevin Templeton <templeton.kevin@gmail.com>
2023-01-17 03:56:48 -0600

    finished routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,8 @@
+const Router = require("express").Router;
+const router = new Router();
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,7 +15,18 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
-
+router.get("/:id", async function (req, res, next) {
+    try {
+        const msg = await Message.get(req.params.id);
+        // current user must match either from_user or to_user of this message to gain access
+        // can't use ensureCorrectUser() because it expects username to be in req.params
+        if (req.user.username === msg.from_user.username || req.user.username === msg.to_user.username) {
+            return res.json(msg);
+        } else {
+            return next({ status: 401, message: "Unauthorized" });
+        }
+    } catch (e) { return next(e); }
+});
 
 /** POST / - post message.
  *
@@ -18,7 +34,14 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
-
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+    try {
+        const { to_username, body } = req.body;
+        const from_username = req.user.username;
+        msg = await Message.create({ from_username, to_username, body });
+        return res.json({ message: msg });
+    } catch (e) { return next(e); }
+});
 
 /** POST/:id/read - mark message as read:
  *
@@ -27,4 +50,15 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
-
+router.post("/:id/read", async function (req, res, next) {
+    try {
+        const msg = await Message.get(req.params.id);
+        // current user must match to_user of this message to gain access
+        // can't use ensureCorrectUser() because it expects username to be in req.params
+        if (req.user.username === msg.to_user.username) {
+            const readMsg = await Message.markRead(req.params.id);
+        } else {
+            return next({ status: 401, message: "Unauthorized" });
+        }
+    } catch (e) { return next(e); }
+});
