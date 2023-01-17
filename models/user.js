746cf7c73c069f2b6fcae7f34586f8243387cf96
Kevin Templeton <templeton.kevin@gmail.com>
2023-01-16 22:49:33 -0600

    User.get() done and refactored to return instances

--- models/user.js
+++ models/user.js
@@ -8,12 +8,21 @@ const { BCRYPT_WORK_FACTOR } = require("../config");
 /** User of the site. */
 
 class User {
+  constructor({ username, password, firstName, lastName, phone, joinAt, lastLoginAt }) {
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.joinAt = joinAt;
+    this.lastLoginAt = lastLoginAt;
+  }
 
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({ username, password, first_name, last_name, phone }) {
+  static async register({ username, password, firstName, lastName, phone }) {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const result = await db.query(
         `INSERT INTO users (
@@ -22,12 +31,13 @@ class User {
               first_name,
               last_name,
               phone,
-              join_at)
-            VALUES ($1, $2, $3, $4, $5, current_timestamp)
-            RETURNING username, password, first_name, last_name, phone`,
-        [username, hashedPassword, first_name, last_name, phone]);
+              join_at,
+              last_login_at)
+            VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+            RETURNING username, password, first_name, last_name, phone, join_at, last_login_at`,
+        [username, hashedPassword, firstName, lastName, phone]);
 
-    return result.rows[0];
+    return new User(result.rows[0]);
   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
@@ -35,9 +45,7 @@ class User {
   static async authenticate(username, password) {
     const result = await db.query(`SELECT username, password FROM users WHERE username = $1`, [username]);
     const user = result.rows[0];
-    if (!user) { return false };
-    if (await bcrypt.compare(password, user.password) === true) { return true };
-    return false;
+    return user && await bcrypt.compare(password, user.password);
   }
 
   /** Update last_login_at for user */
@@ -53,8 +61,8 @@ class User {
    * [{username, first_name, last_name, phone}, ...] */
 
   static async all() {
-    const results = await db.query('SELECT username, first_name, last_name, phone FROM users');
-    return results.rows;
+    const results = await db.query(`SELECT username, first_name, last_name, phone FROM users`);
+    return results.rows.map(u => new User(u));
   }
 
   /** Get: get user by username
@@ -66,7 +74,18 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(`SELECT username, first_name, last_name, phone, join_at, last_login_at FROM users WHERE username=$1`, [username]);
+    const user = results.rows[0];
+
+    if (user === undefined) {
+      const err = new Error(`No such user: ${username}`);
+      err.status = 404;
+      throw err;
+    }
+
+    return new User(user);
+  }
 
   /** Return messages from this user.
    *
